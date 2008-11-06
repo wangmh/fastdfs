5d221fb55b926c840e6ada44e53467f474ed0eea
happyfish100 <happyfish100@0d4c4929-bc51-0410-a161-b7c81ae5c018>
2008-11-06 10:24:58 +0000

    scan_row_count
    git-svn-id: http://fastdfs.googlecode.com/svn@184 0d4c4929-bc51-0410-a161-b7c81ae5c018

--- trunk/storage/storage_sync.c
+++ trunk/storage/storage_sync.c
@@ -1535,7 +1535,8 @@ static void* storage_sync_thread_entrance(void* arg)
 			}
 
 			reader.binlog_offset += record_len;
-			if (++reader.sync_row_count % 1000 == 0)
+			reader.scan_row_count++;
+			if (reader.sync_row_count % 1000 == 0)
 			{
 				if (storage_write_to_mark_file(&reader) != 0)
 				{
