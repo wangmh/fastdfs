f965cc61e106b25e6b3c125dfd014780e03e8658
happyfish100 <happyfish100@0d4c4929-bc51-0410-a161-b7c81ae5c018>
2008-12-15 10:10:15 +0000

    sched_thread.h /.c support time_base of task
    git-svn-id: http://fastdfs.googlecode.com/svn@237 0d4c4929-bc51-0410-a161-b7c81ae5c018

--- trunk/common/common_define.h
+++ trunk/common/common_define.h
@@ -66,6 +66,7 @@ extern int pthread_mutexattr_settype(pthread_mutexattr_t *attr, int kind);
 #define DEFAULT_NETWORK_TIMEOUT			30
 #define DEFAULT_MAX_CONNECTONS			256
 #define SYNC_LOG_BUFF_DEF_INTERVAL              10
+#define TIME_NONE                               -1
 
 #define IP_ADDRESS_SIZE	16
 
