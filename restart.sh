3987d4636ab9ce966ba8aa5d1d8f81a3dfa2c4b1
happyfish100 <happyfish100@0d4c4929-bc51-0410-a161-b7c81ae5c018>
2009-04-07 09:01:41 +0000

    change restart.sh

--- restart.sh
+++ restart.sh
@@ -5,16 +5,6 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-param=''
-list='2 3 4 5 6 7 8 9'
-for i in $list; do
-  eval p='$'$i
-  if [ -z $p ]; then
-    break
-  fi
-  param="$param $p"
-done
-
 if [ -f /bin/awk ]; then
   AWK=/bin/awk
 else
@@ -34,21 +24,45 @@ else
 fi
 
 program=`/bin/echo $1 | $AWK -F '/' '{print $NF;}'`
-count=`/bin/ps auxww | $GREP -w $program | $GREP -v grep | /usr/bin/wc -l`
-if [ $count -gt 0 ]; then
+param=''
+grep_cmd="$GREP -w $program"
+
+list='2 3 4 5 6 7 8 9'
+for i in $list; do
+  eval p='$'$i
+  if [ -z "$p" ]; then
+    break
+  fi
+  param="$param $p"
+  grep_cmd="$grep_cmd | $GREP -w $p"
+done
+cmd="/bin/ps auxww | $grep_cmd | $GREP -v grep | $AWK '{print \$2;}'"
+echo "$cmd"
+pids=`$cmd`
+if [ -z "$pids" ]; then
   i=0
+  count=0
   /bin/echo "stopping $program ..."
   while [ 1 -eq 1 ]; do
-    if [ $i -eq 0 ]; then
-       /usr/bin/killall $program
-    else
-       /usr/bin/killall $program >/dev/null 2>&1
-    fi
+    new_pids=''
+    for pid in $pids; do
+        if [ $i -eq 0 ]; then
+           /bin/kill $pid
+        else
+           /bin/kill $pid >/dev/null 2>&1
+        fi
 
-    if [ $? -ne 0 ]; then
+        count=`$EXPR $count + 1`
+    	if [ $? -eq 0 ]; then
+           new_pids="$new_pids $pid"
+    	fi
+    done
+
+    if [ -z "$new_pids" ]; then
       break
     fi
 
+    pids="$new_pids"
     /usr/bin/printf .
     /bin/sleep 1
     i=`$EXPR $i + 1`
@@ -56,12 +70,12 @@ if [ $count -gt 0 ]; then
 fi
 
 /bin/echo ""
-count=`/bin/ps auxww | $GREP -w $program | $GREP -v grep | $GREP -v $0 | /usr/bin/wc -l`
+count=`/bin/ps auxww | $grep_cmd | $GREP -v grep | $GREP -v $0 | /usr/bin/wc -l`
 if [ $count -eq 0 ]; then
   /bin/echo "starting $program ..."
   exec $1 $param
 else
-  /bin/ps auxww | $GREP -w $program | $GREP -v grep | $GREP -v $0
+  /bin/ps auxww | $grep_cmd | $GREP -v grep | $GREP -v $0
   /bin/echo "already running $program count: $count, restart aborted!"
 fi
 
