20efc13f77ea195fd97fb26b51d4f8a66c6df33d
happyfish100 <happyfish100@0d4c4929-bc51-0410-a161-b7c81ae5c018>
2009-10-07 07:36:25 +0000

    support file id

--- php_client/fastdfs_client.h
+++ php_client/fastdfs_client.h
@@ -25,6 +25,9 @@ ZEND_FUNCTION(fastdfs_tracker_query_storage_store);
 ZEND_FUNCTION(fastdfs_tracker_query_storage_update);
 ZEND_FUNCTION(fastdfs_tracker_query_storage_fetch);
 ZEND_FUNCTION(fastdfs_tracker_query_storage_list);
+ZEND_FUNCTION(fastdfs_tracker_query_storage_update1);
+ZEND_FUNCTION(fastdfs_tracker_query_storage_fetch1);
+ZEND_FUNCTION(fastdfs_tracker_query_storage_list1);
 
 PHP_FASTDFS_API zend_class_entry *php_fdfs_get_ce(void);
 PHP_FASTDFS_API zend_class_entry *php_fdfs_get_exception(void);
--- php_client/fastdfs_test.php
+++ php_client/fastdfs_test.php
@@ -1,6 +1,7 @@
 <?php
  $group_name = "group1";
  $remote_filename = "M00/28/E3/U6Q-CkrMFUgAAAAAAAAIEBucRWc5452.h";
+ $file_id = $group_name . FDFS_FILE_ID_SEPERATOR . $remote_filename;
 
  $tracker = fastdfs_tracker_get_connection();
  var_dump($tracker);
@@ -13,7 +14,11 @@
  //var_dump(fastdfs_tracker_query_storage_store());
  //var_dump(fastdfs_tracker_query_storage_update($group_name, $remote_filename));
  //var_dump(fastdfs_tracker_query_storage_fetch($group_name, $remote_filename));
- var_dump(fastdfs_tracker_query_storage_list($group_name, $remote_filename));
+ //var_dump(fastdfs_tracker_query_storage_list($group_name, $remote_filename));
+
+ var_dump(fastdfs_tracker_query_storage_update1($file_id));
+ var_dump(fastdfs_tracker_query_storage_fetch1($file_id));
+ var_dump(fastdfs_tracker_query_storage_list1($file_id));
 
  $fdfs = new FastDFS();
  $tracker = $fdfs->tracker_get_connection();
@@ -27,6 +32,9 @@
  //var_dump($fdfs->tracker_query_storage_store());
  //var_dump($fdfs->tracker_query_storage_update($group_name, $remote_filename));
  //var_dump($fdfs->tracker_query_storage_fetch($group_name, $remote_filename));
- var_dump($fdfs->tracker_query_storage_list($group_name, $remote_filename));
+ //var_dump($fdfs->tracker_query_storage_list($group_name, $remote_filename));
 
+ var_dump($fdfs->tracker_query_storage_update1($file_id));
+ var_dump($fdfs->tracker_query_storage_fetch1($file_id));
+ var_dump($fdfs->tracker_query_storage_list1($file_id));
 ?>
