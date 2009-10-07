33973c25231298a5fc54a6e2ed431a71e80e55b3
happyfish100 <happyfish100@0d4c4929-bc51-0410-a161-b7c81ae5c018>
2009-10-07 04:24:47 +0000

    tracker php ext

--- php_client/fastdfs_client.h
+++ php_client/fastdfs_client.h
@@ -21,6 +21,9 @@ ZEND_FUNCTION(fastdfs_tracker_get_connection);
 ZEND_FUNCTION(fastdfs_connect_server);
 ZEND_FUNCTION(fastdfs_disconnect_server);
 ZEND_FUNCTION(fastdfs_tracker_list_groups);
+ZEND_FUNCTION(fastdfs_tracker_query_storage_store);
+ZEND_FUNCTION(fastdfs_tracker_query_storage_update);
+ZEND_FUNCTION(fastdfs_tracker_query_storage_fetch);
 
 PHP_FASTDFS_API zend_class_entry *php_fdfs_get_ce(void);
 PHP_FASTDFS_API zend_class_entry *php_fdfs_get_exception(void);
--- php_client/fastdfs_test.php
+++ php_client/fastdfs_test.php
@@ -1,4 +1,7 @@
 <?php
+ $group_name = "group1";
+ $remote_filename = "M00/28/E3/U6Q-CkrMFUgAAAAAAAAIEBucRWc5452.h";
+
  $tracker = fastdfs_tracker_get_connection();
  var_dump($tracker);
 
@@ -6,7 +9,10 @@
  var_dump($server);
  var_dump(fastdfs_disconnect_server($server));
 
- var_dump(fastdfs_tracker_list_groups());
+ //var_dump(fastdfs_tracker_list_groups());
+ //var_dump(fastdfs_tracker_query_storage_store());
+ var_dump(fastdfs_tracker_query_storage_update($group_name, $remote_filename));
+ var_dump(fastdfs_tracker_query_storage_fetch($group_name, $remote_filename));
 
  $fdfs = new FastDFS();
  $tracker = $fdfs->tracker_get_connection();
@@ -15,4 +21,10 @@
  $server = $fdfs->connect_server($tracker['ip_addr'], $tracker['port']);
  var_dump($server);
  var_dump($fdfs->disconnect_server($server));
+
+ //var_dump($fdfs->tracker_list_groups());
+ //var_dump($fdfs->tracker_query_storage_store());
+ var_dump($fdfs->tracker_query_storage_update($group_name, $remote_filename));
+ var_dump($fdfs->tracker_query_storage_fetch($group_name, $remote_filename));
+
 ?>
