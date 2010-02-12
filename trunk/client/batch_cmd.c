c92553f59c99af808dab04fc5cb505fed8551881
happyfish100 <happyfish100@0d4c4929-bc51-0410-a161-b7c81ae5c018>
2010-02-12 04:02:02 +0000

    batch
    git-svn-id: http://fastdfs.googlecode.com/svn@470 0d4c4929-bc51-0410-a161-b7c81ae5c018

--- trunk/client/batch_cmd.c
+++ trunk/client/batch_cmd.c
@@ -9,29 +9,48 @@
 #include "ini_file_reader.h"
 
 #define CONF_FILE "./batch_cmd.conf"
+#define MAX_ARG_COUNT 32
+#define MALLOC_BUFF_ONCE     (4 * 1024)
+
+/*
+typedef struct {
+	int f_read;
+	int f_write;
+} PipeFD;
+*/
+
+static int *f_ins;
+static int *f_outs;
+static int *f_errs;
+
+static char stdin_buff[1024];
+static BufferInfo *stdout_buff;
+static BufferInfo *stderr_buff;
+
+static bool *read_in;
+static bool *read_out;
+static bool *read_err;
+
+static IniItem *hosts;
+static IniItem *pHostEnd;
+static int host_count;
+
+static int do_init();
+static int batch_exec(char * args[]);
+static int proccess_io();
 
 int main(int argc,char* argv[])
 {
-	int f_ins[2];  //f_ins[0]:for read, f_ins[1]:for write.
-	int f_outs[2];  //f_outs[0]:for read, f_ins[1]:for write.
-	int f_errs[2];  //f_errs[0]:for read, f_ins[1]:for write.
-	int pid;
-	char stdin_buff[1024];
-	char stdout_buff[1024];
-	char stderr_buff[1024];
-	char read_in;
-	char read_out;
-	char read_err;
 	int result;
-	int bytes;
 	int i;
-	char *args[64];
+	char *args[MAX_ARG_COUNT];
 	char *group_name;
 	IniContext iniContext;
+	IniItem *pHost;
 
-	if (argc < 3 || argc > 64)
+	if (argc < 3 || argc >= MAX_ARG_COUNT)
 	{
-		printf("Usage: %s <comand line>\n", argv[0]);
+		printf("Usage: %s <group_name> <comand line>\n", argv[0]);
 		return 1;
 	}
 
@@ -47,142 +66,364 @@ int main(int argc,char* argv[])
 
 	group_name = argv[1];
 
-	iniPrintItems(&iniContext);
-	exit(0);
+	//iniPrintItems(&iniContext);
 
-	/*
-	IniItem *iniGetValuesEx(const char *szSectionName, const char *szItemName, \
-		IniContext *pContext, int *nTargetCount);
-	*/
+	hosts = iniGetValuesEx(group_name, "host", &iniContext, &host_count);
+	if (hosts == NULL)
+	{
+		fprintf(stderr, "file: "__FILE__", line: %d, " \
+			"host of section: \"%s\" not exist\n", \
+			__LINE__, group_name);
+		return ENOENT;
+	}
 
-	iniFreeContext(&iniContext);
+	printf("host count: %d\n", host_count);
+	pHostEnd = hosts + host_count;
+	for (pHost=hosts; pHost<pHostEnd; pHost++)
+	{
+		printf("%s\n", pHost->value);
+	}
+
+	args[0] = "ssh";
 
 	for (i=2; i<argc; i++)
 	{
-		args[i-2] = argv[i];
+		args[i] = argv[i];
 	}
-	args[argc-2] = NULL;
+	args[argc] = NULL;
 
-
- if(pipe(f_ins)==-1)
- {
-  perror("Fail to creat pipe.\n");
-  return 1;
- }
-
- if(pipe(f_outs)==-1)
- {
-  perror("Fail to creat pipe.\n");
-  return 1;
- }
-
- if(pipe(f_errs)==-1)
- {
-  perror("Fail to creat pipe.\n");
-  return 1;
- }
-
- if((pid=fork())==-1)
- {
-  perror("Fail to creat child process.\n");
-  return 1;
- }
-
- if(pid==0)
- {
-  dup2(f_ins[0], STDIN_FILENO);
-  dup2(f_outs[1], STDOUT_FILENO);
-  dup2(f_errs[1], STDERR_FILENO);
-
-  close(f_ins[0]);
-  close(f_ins[1]);
-
-  close(f_outs[0]);
-  close(f_outs[1]);
-
-  close(f_errs[0]);
-  close(f_errs[1]);
-
-  if(execvp(argv[2], args)==-1)
-  {
-   printf("Child process can't exec command %s.\n",argv[1]);
-   return 1;
-  }
-  _exit(0);
- }
-
-	close(f_ins[0]);
-	close(f_outs[1]);
-	close(f_errs[1]);
-
-	if (set_nonblock(STDIN_FILENO) < 0)
+	if ((result=do_init()) != 0)
 	{
-		return 1;
+		return result;
 	}
 
-	if (set_nonblock(f_outs[0]) < 0)
+	if ((result=batch_exec(args)) != 0)
 	{
-		return 1;
+		return result;
 	}
 
-	if (set_nonblock(f_errs[0]) < 0)
+	if ((result=proccess_io()) != 0)
 	{
-		return 1;
+		return result;
 	}
 
-	read_in = 1;
-	read_out = 1;
-	read_err = 1;
+	iniFreeContext(&iniContext);
+	return 0;
+}
 
-	while (read_out || read_err)
+static int do_init()
+{
+	f_ins = (int *)malloc(sizeof(int) * host_count);
+	if (f_ins == NULL)
 	{
-		sleep(1);
+		fprintf(stderr, "file: "__FILE__", line: %d, " \
+			"malloc %d bytes fail, " \
+			"errno: %d, error info: %s\n", __LINE__, \
+			(int)sizeof(int) * host_count, \
+			errno, strerror(errno));
+		return errno !=0 ? errno : ENOMEM;
+	}
 
-		while (read_err)
+	f_outs = (int *)malloc(sizeof(int) * host_count);
+	if (f_outs == NULL)
+	{
+		fprintf(stderr, "file: "__FILE__", line: %d, " \
+			"malloc %d bytes fail, " \
+			"errno: %d, error info: %s\n", __LINE__, \
+			(int)sizeof(int) * host_count, \
+			errno, strerror(errno));
+		return errno !=0 ? errno : ENOMEM;
+	}
+
+	f_errs = (int *)malloc(sizeof(int) * host_count);
+	if (f_errs == NULL)
+	{
+		fprintf(stderr, "file: "__FILE__", line: %d, " \
+			"malloc %d bytes fail, " \
+			"errno: %d, error info: %s\n", __LINE__, \
+			(int)sizeof(int) * host_count, \
+			errno, strerror(errno));
+		return errno !=0 ? errno : ENOMEM;
+	}
+
+	stdout_buff = (BufferInfo *)malloc(sizeof(BufferInfo) * host_count);
+	if (stdout_buff == NULL)
+	{
+		fprintf(stderr, "file: "__FILE__", line: %d, " \
+			"malloc %d bytes fail, " \
+			"errno: %d, error info: %s\n", __LINE__, \
+			(int)sizeof(BufferInfo) * host_count, \
+			errno, strerror(errno));
+		return errno !=0 ? errno : ENOMEM;
+	}
+	memset(stdout_buff, 0, sizeof(BufferInfo) * host_count);
+
+	stderr_buff = (BufferInfo *)malloc(sizeof(BufferInfo) * host_count);
+	if (stderr_buff == NULL)
+	{
+		fprintf(stderr, "file: "__FILE__", line: %d, " \
+			"malloc %d bytes fail, " \
+			"errno: %d, error info: %s\n", __LINE__, \
+			(int)sizeof(BufferInfo) * host_count, \
+			errno, strerror(errno));
+		return errno !=0 ? errno : ENOMEM;
+	}
+	memset(stderr_buff, 0, sizeof(BufferInfo) * host_count);
+
+	read_in = (bool *)malloc(sizeof(bool) * host_count);
+	if (read_in == NULL)
+	{
+		fprintf(stderr, "file: "__FILE__", line: %d, " \
+			"malloc %d bytes fail, " \
+			"errno: %d, error info: %s\n", __LINE__, \
+			(int)sizeof(bool) * host_count, \
+			errno, strerror(errno));
+		return errno !=0 ? errno : ENOMEM;
+	}
+	memset(read_in, 1, sizeof(bool) * host_count);
+
+	read_out = (bool *)malloc(sizeof(bool) * host_count);
+	if (read_out == NULL)
+	{
+		fprintf(stderr, "file: "__FILE__", line: %d, " \
+			"malloc %d bytes fail, " \
+			"errno: %d, error info: %s\n", __LINE__, \
+			(int)sizeof(bool) * host_count, \
+			errno, strerror(errno));
+		return errno !=0 ? errno : ENOMEM;
+	}
+	memset(read_out, 1, sizeof(bool) * host_count);
+
+	read_err = (bool *)malloc(sizeof(bool) * host_count);
+	if (read_err == NULL)
+	{
+		fprintf(stderr, "file: "__FILE__", line: %d, " \
+			"malloc %d bytes fail, " \
+			"errno: %d, error info: %s\n", __LINE__, \
+			(int)sizeof(bool) * host_count, \
+			errno, strerror(errno));
+		return errno !=0 ? errno : ENOMEM;
+	}
+	memset(read_err, 1, sizeof(bool) * host_count);
+
+	return 0;
+}
+
+static int batch_exec(char * args[])
+{
+	IniItem *pHost;
+	int pid;
+	int in_fds[2];
+	int err_fds[2];
+	int out_fds[2];
+	int i;
+	int result;
+
+	for (pHost=hosts, i=0; pHost<pHostEnd; pHost++, i++)
+	{
+		printf("%s\n", pHost->value);
+		if(pipe(in_fds) < 0)
 		{
-			bytes = read(f_errs[0], stderr_buff, sizeof(stderr_buff) - 1);
+			fprintf(stderr, "file: "__FILE__", line: %d, " \
+				"call pipe fail, " \
+				"errno: %d, error info: %s\n", __LINE__, \
+				errno, strerror(errno));
+			return errno != 0 ? errno : EMFILE;
+		}
+
+		if(pipe(err_fds) < 0)
+		{
+			fprintf(stderr, "file: "__FILE__", line: %d, " \
+				"call pipe fail, " \
+				"errno: %d, error info: %s\n", __LINE__, \
+				errno, strerror(errno));
+			return errno != 0 ? errno : EMFILE;
+		}
+
+
+		if(pipe(out_fds) < 0)
+		{
+			fprintf(stderr, "file: "__FILE__", line: %d, " \
+				"call pipe fail, " \
+				"errno: %d, error info: %s\n", __LINE__, \
+				errno, strerror(errno));
+			return errno != 0 ? errno : EMFILE;
+		}
+
+		if((pid=fork()) < 0)
+		{
+			fprintf(stderr, "file: "__FILE__", line: %d, " \
+				"call fork fail, " \
+				"errno: %d, error info: %s\n", __LINE__, \
+				errno, strerror(errno));
+			return errno != 0 ? errno : ENOMEM;
+		}
+
+		if(pid == 0) //child process
+		{
+			dup2(in_fds[0], STDIN_FILENO);
+			dup2(out_fds[1], STDOUT_FILENO);
+			dup2(err_fds[1], STDERR_FILENO);
+
+			close(in_fds[0]);
+			close(in_fds[1]);
+
+			close(out_fds[0]);
+			close(out_fds[1]);
+
+			close(err_fds[0]);
+			close(err_fds[1]);
+
+			args[1] = pHost->value;
+			if(execvp(args[0], args) < 0)
+			{
+				fprintf(stderr, "file: "__FILE__", line: %d, " \
+					"call execvp fail, " \
+					"errno: %d, error info: %s\n", \
+					__LINE__, errno, strerror(errno));
+  			}
+			exit(0);
+ 		}
+
+		f_ins[i] = in_fds[1];
+		f_outs[i] = out_fds[0];
+		f_errs[i] = err_fds[0];
+
+		if ((result=set_nonblock(f_outs[i])) != 0)
+		{
+			return result;
+		}
+
+		if ((result=set_nonblock(f_errs[i])) != 0)
+		{
+			return result;
+		}
+
+		close(in_fds[0]);
+		close(out_fds[1]);
+		close(err_fds[1]);
+	}
+
+	return 0;
+}
+
+#define CHECK_BUFF_SIZE(bf) \
+	if (bf.alloc_size - bf.length <= 1) \
+	{ \
+		bf.alloc_size += MALLOC_BUFF_ONCE; \
+		bf.buff = (char *)realloc(bf.buff, bf.alloc_size); \
+		if (bf.buff == NULL) \
+		{ \
+			fprintf(stderr, "file: "__FILE__", line: %d, " \
+				"malloc %d bytes fail, " \
+				"errno: %d, error info: %s\n", __LINE__, \
+				bf.alloc_size, errno, strerror(errno)); \
+			return errno != 0 ? errno : ENOMEM; \
+		} \
+	}
+
+
+static int proccess_io()
+{
+	IniItem *pHost;
+	int bytes;
+	int result;
+	int i;
+	int k;
+	int in_count;
+	int out_count;
+	int err_count;
+
+	if ((result=set_nonblock(STDIN_FILENO)) != 0)
+	{
+		return result;
+	}
+
+	in_count = host_count;
+	out_count = host_count;
+	err_count = host_count;
+
+	while (out_count > 0 || err_count > 0)
+	{
+	sleep(1);
+
+	for (pHost=hosts, i=0; pHost<pHostEnd; pHost++, i++)
+	{
+		while (read_err[i])
+		{
+			CHECK_BUFF_SIZE((stderr_buff[i]))
+
+			bytes = read(f_errs[i], stderr_buff[i].buff + \
+				stderr_buff[i].length, \
+				stderr_buff[i].alloc_size - \
+				stderr_buff[i].length - 1);
 			if (bytes < 0)
 			{
 				if (!(errno == EAGAIN || errno == EWOULDBLOCK))
 				{
 					fprintf(stderr, "read error, bytes=%d, errno=%d\n", bytes, errno);
-					read_err = 0;
+					read_err[i] = 0;
+					err_count--;
 				}
 
 				break;
 			}
 			else if (bytes == 0)
 			{
-				read_err = 0;
+				read_err[i] = 0;
+				err_count--;
+				break;
 			}
 
-			*(stderr_buff + bytes) = '\0';
-			printf("%s", stderr_buff);
+			if (i == 0)
+			{
+				*(stderr_buff[i].buff + stderr_buff[i].length \
+					+ bytes) = '\0';
+				printf("%s", stderr_buff[i].buff + \
+					stderr_buff[i].length);
+			}
+
+			stderr_buff[i].length += bytes;
 		}
 
-		while (read_out)
+		while (read_out[i])
 		{
-			bytes = read(f_outs[0], stdout_buff, sizeof(stdout_buff) - 1);
+			CHECK_BUFF_SIZE((stdout_buff[i]))
+
+			bytes = read(f_outs[i], stdout_buff[i].buff + \
+				stdout_buff[i].length, \
+				stdout_buff[i].alloc_size - \
+				stdout_buff[i].length - 1);
 			if (bytes < 0)
 			{
 				if (!(errno == EAGAIN || errno == EWOULDBLOCK))
 				{
 					fprintf(stderr, "read error, bytes=%d, errno=%d\n", bytes, errno);
-					read_out = 0;
+					read_out[i] = 0;
+					out_count--;
 				}
 
 				break;
 			}
 			else if (bytes == 0)
 			{
-				read_out = 0;
+				read_out[i] = 0;
+				out_count--;
+				break;
 			}
 
-			*(stdout_buff + bytes) = '\0';
-			printf("%s", stdout_buff);
+			if (i == 0)
+			{
+				*(stdout_buff[i].buff + stdout_buff[i].length \
+					+ bytes) = '\0';
+				printf("%s", stdout_buff[i].buff + \
+						stdout_buff[i].length);
+			}
+
+			stdout_buff[i].length += bytes;
 		}
 
-		while (read_in)
+		while (in_count > 0)
 		{
 			bytes = read(STDIN_FILENO, stdin_buff, sizeof(stdin_buff));
 			if (bytes < 0)
@@ -190,29 +431,35 @@ int main(int argc,char* argv[])
 				if (!(errno == EAGAIN || errno == EWOULDBLOCK))
 				{
 					fprintf(stderr, "read bytes=%d, errno=%d\n", bytes, errno);
-					read_in = 0;
 				}
 
 				break;
 			}
 			else if (bytes == 0)
 			{
-				read_in = 0;
 				break;
 			}
 
-			if (write(f_ins[1], stdin_buff, bytes) != bytes)
+			*(stdin_buff + bytes) = '\0';
+			printf("stdin_buff=%s\n", stdin_buff);
+
+			for (k=0; k<host_count; k++)
 			{
-				fprintf(stderr, "write bytes=%d, errno=%d\n", bytes, errno);
-				read_in = 0;
-				break;
+				if (!read_in[k])
+				{
+					continue;
+				}
+
+				if (write(f_ins[k], stdin_buff, bytes) != bytes)
+				{
+					fprintf(stderr, "write bytes=%d, errno=%d\n", bytes, errno);
+					read_in[k] = 0;
+					in_count--;
+				}
 			}
 		}
 	}
-
-	close(f_ins[1]);
-	close(f_outs[0]);
-	close(f_errs[0]);
+	}
 
 	return 0;
 }
