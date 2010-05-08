e78cbdbe2feba5c24c1d06c09a46918eef2fc920
happyfish100 <happyfish100@0d4c4929-bc51-0410-a161-b7c81ae5c018>
2010-05-08 07:07:33 +0000

    php ext ok

--- common/fdfs_global.c
+++ common/fdfs_global.c
@@ -21,7 +21,7 @@
 #include "fdfs_global.h"
 
 int g_fdfs_network_timeout = DEFAULT_NETWORK_TIMEOUT;
-char g_fdfs_base_path[MAX_PATH_SIZE];
+char g_fdfs_base_path[MAX_PATH_SIZE] = {'/', 't', 'm', 'p', '\0'};
 Version g_fdfs_version = {1, 28};
 
 /*
