05b64a07c2e89dc60b2389b1f68a6045612e5bb3
happyfish100 <happyfish100@0d4c4929-bc51-0410-a161-b7c81ae5c018>
2010-07-11 11:04:43 +0000

    temp save
    git-svn-id: http://fastdfs.googlecode.com/svn@566 0d4c4929-bc51-0410-a161-b7c81ae5c018

--- trunk/storage/storage_nio.h
+++ trunk/storage/storage_nio.h
@@ -17,6 +17,44 @@
 #include <event.h>
 #include "fast_task_queue.h"
 
+
+#define FDFS_STORAGE_STAGE_NIO_RECV    11
+#define FDFS_STORAGE_STAGE_NIO_SEND    12
+#define FDFS_STORAGE_STAGE_DIO_READ    21
+#define FDFS_STORAGE_STAGE_DIO_WRITE   22
+
+#define FDFS_STORAGE_FILE_OP_WRITE    'w'
+#define FDFS_STORAGE_FILE_OP_READ     'r'
+
+typedef struct
+{
+	int thread_index;
+	int stage;
+	int sock;
+	char ip_addr[IP_ADDRESS_SIZE];  //to be removed
+	char tracker_client_ip[IP_ADDRESS_SIZE];
+
+	int64_t total_length;   //pkg total length
+	int64_t total_offset;   //pkg current offset
+
+	char sync_flag;
+	char file_op;     //w for writing, r for reading
+	int fd;           //file description no
+	int64_t file_size;   //file size
+	int64_t file_offset; //file offset
+
+	int src_sync_timestamp;
+	FDFSStorageServer *pSrcStorage;
+} StorageClientInfo;
+
+struct storage_thread_data
+{
+        struct event_base *ev_base;
+        int pipe_fds[2];
+        int dealing_file_count;
+	GroupArray group_array;
+};
+
 #ifdef __cplusplus
 extern "C" {
 #endif
