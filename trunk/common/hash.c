2a7e9c8c1e2eecc574a1cd1a8ae35c6b492685f6
happyfish100 <happyfish100@0d4c4929-bc51-0410-a161-b7c81ae5c018>
2010-11-16 15:48:17 +0000

    one of file hash code signature function change from APHash_ex to simple_hash_ex
    git-svn-id: http://fastdfs.googlecode.com/svn@670 0d4c4929-bc51-0410-a161-b7c81ae5c018

--- trunk/common/hash.c
+++ trunk/common/hash.c
@@ -727,15 +727,15 @@ int JSHash_ex(const void *key, const int key_len, \
 	JS_HASH_FUNC(init_value)
 }
  
+#define BITS_IN_UNIGNED_INT (int)(sizeof(int) * 8)
+#define THREE_QUARTERS      (int)((BITS_IN_UNIGNED_INT * 3) / 4)
+#define HASH_ONE_EIGHTH     (int)(BITS_IN_UNIGNED_INT / 8)
+#define HASH_HIGH_BITS      (int)((unsigned int)(0xFFFFFFFF) << \
+				(BITS_IN_UNIGNED_INT - HASH_ONE_EIGHTH))
+
 #define PJW_HASH_FUNC(init_value) \
     unsigned char *pKey; \
     unsigned char *pEnd; \
-    int BitsInUnignedInt = (int)(sizeof(int) * 8); \
-    int ThreeQuarters    = (int)((BitsInUnignedInt * 3) / 4);\
-    int OneEighth        = (int)(BitsInUnignedInt / 8); \
- \
-    int HighBits         = (int)(0xFFFFFFFF) << \
-				(BitsInUnignedInt - OneEighth); \
     int hash; \
     int test; \
  \
@@ -743,10 +743,10 @@ int JSHash_ex(const void *key, const int key_len, \
     pEnd = (unsigned char *)key + key_len; \
     for (pKey = (unsigned char *)key; pKey != pEnd; pKey++) \
     { \
-        hash = (hash << OneEighth) + (*(pKey)); \
-        if ((test = hash & HighBits) != 0) \
+        hash = (hash << HASH_ONE_EIGHTH) + (*(pKey)); \
+        if ((test = hash & HASH_HIGH_BITS) != 0) \
         { \
-            hash = ((hash ^ (test >> ThreeQuarters)) & (~HighBits)); \
+            hash = ((hash ^ (test >> THREE_QUARTERS)) & (~HASH_HIGH_BITS)); \
         } \
     } \
  \
